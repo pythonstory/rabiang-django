2b98fee88496857613ba5469e22bb33ef0ba8ec2
pythonstory <pincoin.info@gmail.com>
2016-08-15 02:50:55 +0900

    Post 모델 추가

---
+++ TODO.md
@@ -0,0 +1,9 @@
+- route
+- ORM
+- form / validation
+- image / file upload
+- i18n
+- themes
+- logging
+- testing
+- admin customizing
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Post(TimeStampedModel):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    author = models.ForeignKey(User, related_name="posts")
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES,
+                              default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
