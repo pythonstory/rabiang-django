c7b4e6cb5da806f8faa214b5b5a5b13a259de9db
pythonstory <pincoin.info@gmail.com>
2016-08-16 16:09:33 +0900

    블로그 게시물 목록/상세보기

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset() \
+            .filter(status='published')
 
 
 class NamedModel(models.Model):
@@ -23,6 +30,10 @@ class Post(TimeStampedModel):
         ('draft', 'Draft'),
         ('published', 'Published')
     )
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
                             unique_for_date='publish',
@@ -36,6 +47,14 @@ class Post(TimeStampedModel):
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        self.reverse = reverse('blog:post_detail',
+                               args=[self.publish.year,
+                                     self.publish.strftime('%m'),
+                                     self.publish.strftime('%d'),
+                                     self.slug])
+        return self.reverse
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year, publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
